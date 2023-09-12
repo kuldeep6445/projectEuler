3e035d858c851844007b5a455ae415fd40c298e6
kuldeep6445 <kuldeepbhogal111@gmail.com>
2023-09-12 22:10:15 +0530

    yo

--- 4.cpp
+++ 4.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool pall(string str){
+    for(int i=0;i<str.size();i++){
+        if(str[i]!=str[str.size()-1-i])
+            return false;
+    }
+    cout<<str;
+    return true;
+}
+
 int main(){
-    
+    string str;
+    for(int i=999;i>100;i--){
+        for(int j=999;j>100;j--){
+            if(pall(to_string(i*j)))
+                return 0;
+        }
+    }
 }
